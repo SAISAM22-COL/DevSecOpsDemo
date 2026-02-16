83bc179567a46e73c27bb29a9ec0c686cce9e779
SAISAMARTH RAJENDRAKUMAR JADHAV <1MS23CS160@msrit.edu>
2026-02-16 07:20:08 +0000

    Pythons tests workflow

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@
 import pytest 
 from calculator import add, subtract, multiply, divide
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 6
     assert add(-1, 1) == 0
 
 def test_subtract():
@@ -16,7 +16,7 @@ def test_multiply():
 
 def test_divide():
     assert divide(10, 2) == 5
-    
+
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         divide(10, 0)
